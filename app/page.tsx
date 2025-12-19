e2e4567d1d04ee3c85f9db76dcda77dac19e1f38
Ny Hasina Vagno <hasina.vagno@esiroi.re>
2025-12-19 21:17:44 +0400

    chore: install intl for internationalization

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
 import { NavigationMenu } from "@/components/navigation/menu";
+import en_message from "@/public/dictionnaries/en.json";
+import fr_message from "@/public/dictionnaries/fr.json";
+import { useState } from "react";
+import { IntlProvider } from "react-intl";
+
+type Messages = {};
+type Locale = "en" | "fr";
+
+const messages: Record<Locale, Messages> = {
+  en: en_message,
+  fr: fr_message,
+};
 
 export default function Home() {
+  const [locale, setLocale] = useState<Locale>("en");
+
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLocale(e.target.value as Locale);
+  };
+
   return (
     <div>
-      <NavigationMenu />
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <NavigationMenu />
+      </IntlProvider>
     </div>
   );
 }
